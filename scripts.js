24e0c7531614fd2e98f9aea82dbe54b0df60ba03
PetarKosev <pkosev91@gmail.com>
2020-05-01 11:25:29 +0300

    Structure different parts

--- scripts.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector('header');
-const { offsetHeight: headerHeight } = header;
-
-document.addEventListener('scroll', () => {
-    header.className = document.documentElement.scrollTop > headerHeight ? 'is-fixed' : '';
-});
